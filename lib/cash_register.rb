1c3a14b1a038c32aace4a3cae9da0e502257b921
junko911 <junkotahara911@gmail.com>
2020-04-01 11:59:12 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,8 +2,23 @@ class CashRegister
     attr_reader :discount
     attr_accessor  :total
 
-    def initialize(discount = 0)
+    def initialize(discount = 20)
         @total = 0
-        @discount = discount
+        @discount = 1 - (discount / 100)
     end
+
+    def add_item(title, price, quantity = 1)
+        @total += price * quantity
+    end
+
+    def apply_discount
+        @total *= @discount
+    end
+
+    def item
+    end
+
+    def void_last_transaction
+    end
+
 end
